463fa1c2159eeef0f9831b7dddd967c80f75261d
Mohamed Darwesh <mohamed_darwesh123@hotmail.com>
2020-05-12 00:38:45 +0200

    Updates

---
+++ precache-manifest.85fe62a352a481a12953d5080740012b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ea24dee04e6d4a75009ec729e5a831",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "adebf6bbe8f91e33c67b",
+    "url": "/website/static/css/main.9b626604.chunk.css"
+  },
+  {
+    "revision": "e14ce482ee04ec89bcf6",
+    "url": "/website/static/js/2.a1652e60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.a1652e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adebf6bbe8f91e33c67b",
+    "url": "/website/static/js/main.1a9dcb63.chunk.js"
+  },
+  {
+    "revision": "60eb781e065599774bd0",
+    "url": "/website/static/js/runtime-main.eb703b0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1fdfa6e36222d25f79043cc0a942d1f4",
+    "url": "/website/static/media/signs.1fdfa6e3.png"
+  }
+]);
